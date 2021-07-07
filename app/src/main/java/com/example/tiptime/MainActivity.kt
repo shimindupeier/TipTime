8f2d1efdb94954e27d60be7872c1c5ee762c07f3
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-07-07 22:41:19 +0100

    Push git repo to github. Tip time first commit

---
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.tiptime
+
+import android.content.Context
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculate.setOnClickListener { calculateTip() }
+
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(
+                view,
+                keyCode
+            )
+        }
+    }
+
+    private fun calculateTip() {
+        // Get the decimal value from cost of service text field
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null || cost == 0.0) {
+            displayTip(0.0, 0.0)
+            return
+        }
+        val tipPercentage = when (binding.tipOption.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        var total = tip + cost
+
+        if (binding.roundUpSwitch.isChecked) {
+            kotlin.math.ceil(total).also { total = it }
+        }
+
+        displayTip(tip, total)
+    }
+
+    private fun displayTip(tip: Double, total: Double) {
+        val formattedTip = NumberFormat.getCurrencyInstance(Locale.UK).format(tip)
+        val formattedTotal = NumberFormat.getCurrencyInstance(Locale.UK).format(total)
+
+        binding.tipAmount.text = getString(R.string.tip_amount, formattedTip)
+        binding.totalAmount.text = getString(R.string.total_amount, formattedTotal)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+}
+
